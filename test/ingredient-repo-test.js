63e798de7ce0035508928514ffa21ea097599f0f
mainlyetcetera <ericcampbell294@gmail.com>
2021-01-10 16:19:29 -0700

    Add can get id by name test

--- test/ingredient-repo-test.js
+++ test/ingredient-repo-test.js
@@ -1,3 +1,9 @@
 import { expect } from 'chai';
 
 import IngredientsRepo from "../src/ingredient-repo";
+
+describe('IngredientRepo', () => {  
+  it("should return ingredients id that matches entered name", () => {
+    expect(ingredients.getRecipeIdByName("butter")).to.eq(1001);
+  });
+});
