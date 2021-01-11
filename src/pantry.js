008b2a4c18128cefa6b8e237d9f3bd945ed340d6
mattdean <matthewallandean@gmail.com>
2021-01-10 20:26:37 -0700

    Pseudocode canMake()

--- src/pantry.js
+++ src/pantry.js
@@ -4,7 +4,14 @@ export default class Pantry {
   }
 
   canMake() {
-    
+    // iterate through recipes ingredients
+    // for each ingredient, check the pantry for at least the amount
+    // if we have it, push it to an "inStock" array
+      // after, iterate over the in stock array and subtract the amount we need for the recipe from this.contents
+    // if we dont, return a string of "cant make, buy more x y z"
+
+
+
   }
 
 };
