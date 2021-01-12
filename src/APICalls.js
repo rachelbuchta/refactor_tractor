07db481a32e34f5da276dc18fb43996b4010ad8a
mainlyetcetera <ericcampbell294@gmail.com>
2021-01-12 11:44:07 -0700

    Start setting up post imports

--- src/APICalls.js
+++ src/APICalls.js
@@ -1,25 +1,28 @@
+
 export const getData = dataset => {
-  let data = [];
-  fetch(`http://localhost:3001/api/v1/${dataset}`)
-    .then(response => response.json())
-    .then(result => {
-      result.forEach(eachResult => {
-        data.push(eachResult)
-      });
-    })
+    let data = [];
+    fetch(`http://localhost:3001/api/v1/${dataset}`)
+      .then(response => response.json())
+      .then(result => {
+        result.forEach(eachResult => {
+          data.push(eachResult)
+        });
+      })
+      .catch(error => console.log('error', error));
+    return data;
+  }
+
+  export const sendData = dataToSend => {
+    const requestOptions = {
+      method: 'POST',
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(dataToSend)
+    };
+
+    fetch("http://localhost:3001/api/v1/users", requestOptions)
+    .then(response => response.text())
+    .then(result => console.log(result))
     .catch(error => console.log('error', error));
-  return data;
-} 
+  }
 
-export const sendData = dataToSend => {
-  const requestOptions = {
-    method: 'POST',
-    headers: { "Content-Type": "application/json" },
-    body: JSON.stringify(dataToSend)
-  };
-
-  fetch("http://localhost:3001/api/v1/users", requestOptions)
-  .then(response => response.text())
-  .then(result => console.log(result))
-  .catch(error => console.log('error', error));
-}
+  export const test = [1, 2, 3];
--- src/data/ingredient-data.js
+++ src/data/ingredient-data.js
@@ -1,10 +1,17 @@
-import getData from '../APICalls';
+import {
+  getData,
+  sendData,
+  test
+ } from '../APICalls';
 
 const apiCall = 'ingredients';
-
 
 let ingredientsData;
 
 ingredientsData = getData(apiCall);
+
+sendData({});
+
+console.log(test);
 
 export default ingredientsData;
